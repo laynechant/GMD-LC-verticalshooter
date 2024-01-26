ef5c24721b754ceb77248f1e1ca0101f24478af6
Layne Chant <layne.chant@gmail.com>
2024-01-26 16:57:52 -0500

    Worked on Game Manager script, and Animated explosion.

--- LC_VerticalShooter/Assets/GameManager.cs
+++ LC_VerticalShooter/Assets/GameManager.cs
@@ -9,19 +9,63 @@ public class GameManager : MonoBehaviour
     public List<GameObject> activeEnemyList; //Create a list (built later from the above array) to store enemies
 
 
-    private void Awake()
+     void Awake() // Awake runs quite early in the game initialization process, ensuring that GameManager will exist before needed by other objects
     {
-        singleton = this; 
+        singleton = this; // inialize the singleton with this object 
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyArray = GameObject.FindGameObjectsWithTag("Enemy"); //load up enemyArray with any objects tagged "Enemy" 
+        InitEnemies(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void InitEnemies() 
+    {
+        activeEnemyList = new List<GameObject>(); // the active enemy list needs to exist so that objects in the list can be removed (a list in c# is just a dynamic array)
+        for (int i = 0; i < enemyArray.Length; i++)
+        {
+            activeEnemyList.Add(enemyArray[i]); // Initalize the activeEnemylist with the enemy array 
+        }
+    }
+
+    void UnlistEnemy(GameObject enemy)
+    {
+        for (int i = 0;i < activeEnemyList.Count; i++)
+        {
+            if (activeEnemyList[i] == enemy)
+            {
+                activeEnemyList.RemoveAt(i); // remove the clicked enemy from the list
+                break; 
+            }
+        }
+        if (activeEnemyList.Count == 0)
+        {
+            StartCoroutine(ResetAllEnemies());
+        }
+    }
+
+    IEnumerator ResetAllEnemies ()
+    {
+        yield return new WaitForSeconds(2);
+        for (int i = 0; i < enemyArray.Length; i++) 
+        {
+            enemyArray[i].GetComponent<Enemy>().Respawn();
+            activeEnemyList.Add(enemyArray[i]);
+        }
+
+    }
+
+    void OnMouseDown () 
+    {
+        Debug.Log("down");
+        gameObject.SetActive(false); // setActive to false (which deactivates the enemys)
+        GameManager.singleton.UnlistEnemy(gameObject);
     }
 }
